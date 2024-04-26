d6b4430ca04cf9adf60398eddea12fd2ddafc5e2
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2024-04-26 19:47:44 -0400

    implemented add product

--- server/api/products_api.py
+++ server/api/products_api.py
@@ -1,4 +1,4 @@
-from flask import (request, jsonify, Blueprint)
+from flask import (request, jsonify, Blueprint, make_response, url_for)
 from .data_store import products
 
 products_blueprint = Blueprint('products_api', __name__, url_prefix='/api/')
@@ -12,3 +12,13 @@ def list_products():
 def get_product(product_id):
     result = next(( product for product in products if product['id'] == product_id), None)
     return jsonify(result)
+
+@products_blueprint.route('/products', methods=['POST'])
+def add_product():
+    data = request.get_json()
+    data['id'] = str(len(products) + 1)
+    products.append(data)
+    location = url_for('.get_product', product_id = data['id'], _extern=True)
+    response = jsonify(dict(product=data, location=location), 201)
+    response.headers['Location'] = location 
+    return response
